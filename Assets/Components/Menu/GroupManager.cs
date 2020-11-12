b7fa526dc10cdf6693e810a8660920f9833af76e
Elyz <elicamodo@hotmail.com>
2020-11-12 11:32:04 -0500

    developer: selectbycollection

--- Assets/Components/Menu/GroupManager.cs
+++ Assets/Components/Menu/GroupManager.cs
@@ -17,7 +17,16 @@ public class GroupManager : MonoBehaviour
 
     public async Task<List<string>> ListNameStudents(string idInductor)
     {
-        return await DataBaseManager.instance.ListNameStudents("Students", idInductor);
+        Dictionary<string, object> data = await DataBaseManager.instance.ListStudentsByGroup("Students", idInductor);
+        List<string> Estudiantes = new List<string>();
+        foreach (KeyValuePair<string, object> pair in data)
+        {
+            if (pair.Key == "name")
+            {
+                Estudiantes.Add(pair.Value.ToString());
+            }        
+        }
+        return Estudiantes;
     }
 
     public async Task<int> SearchCurrentSizeRoom(string idInductor)
@@ -41,5 +50,37 @@ public class GroupManager : MonoBehaviour
     public async Task<Dictionary<string,object>> GetInductorDataAsync(Text inductorNameLabel)
     {
         return await UsersManager.instance.GetUserAsync("Inductors", AuthManager.instance.GetUserId());        
+    }
+
+    public async Task<Dictionary<string, string>> GetOtherGroupsDataAsync()
+    {
+        string nameInductor = "", nameRoom  = "";
+        List<Dictionary<string, object>> rooms = await DataBaseManager.instance.SearchByCollection("Rooms");
+        foreach (Dictionary<string, object> room in rooms)
+        {
+            foreach (KeyValuePair<string, object> pairRoom in room)
+            {
+                if (pairRoom.Key == "room")
+                {
+                    nameRoom = pairRoom.Value.ToString();
+                }
+                else if (pairRoom.Key == "idInductor")
+                {
+                    Dictionary<string, object> inductor = await UsersManager.instance.GetUserAsync("Inductors", pairRoom.Value.ToString());
+                    foreach (KeyValuePair<string, object> pairInductor in inductor)
+                    {   
+                        if (pairRoom.Key == "name")
+                        {
+                            nameInductor = pairInductor.Value.ToString();
+                        }
+                    }
+                }
+            }
+        }
+        
+        return new Dictionary<string, string> () {
+            {"nameInductor", nameInductor},
+            {"nameRoom", nameRoom}
+        }
     }
 }
