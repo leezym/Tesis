039991dc7f50f8089800de28ed4723acde6c7ae5
DESKTOP-8A1G7ND\totor <totoroldan12@gmail.com>
2020-12-27 22:46:13 -0500

    design: Se agregó el script HintsManager, al igual que el traer los diferentes edificios en el dropdown de la pantalla de las Pistas/Hints

---
+++ Assets/Scripts/HintsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+public class HintsManager : MonoBehaviour
+{
+    //public static HintsManager instance;
+    public Dropdown BuildingsDropdown;
+    public Canvas canvasHints;
+    List<Dictionary<string,object>> BuildingsList = new List<Dictionary<string,object>>();
+
+    // Nombre de la DB - Buildings
+    async void Start()
+    {
+        await SearchBuildingByCollection();
+    }
+
+    void Update()
+    {
+    }
+
+    async Task SearchBuildingByCollection(){
+        BuildingsList = await DataBaseManager.instance.SearchByCollection("Buildings");
+        foreach(Dictionary<string,object> Buildings in BuildingsList){
+            foreach(KeyValuePair<string,object> pair in Buildings){
+                if(pair.Key == "name"){
+                    Dropdown.OptionData option = new Dropdown.OptionData();
+                    option.text = pair.Value.ToString();
+                    BuildingsDropdown.options.Add(option);
+                }
+            }
+        }
+    }
+
+}
