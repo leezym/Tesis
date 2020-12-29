c882879ac4bf0c3742c05eae94c538ca2a08bc23
Elizabeth Moncada <elicamodo@hotmail.com>
2020-12-28 21:24:03 -0500

    muestra las pistas en tiempo real

---
+++ Assets/Components/Menu/Activities/HintElement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintElement : MonoBehaviour
+{
+    public void SelectHint()
+    {
+        Text hintText = this.gameObject.GetComponentInChildren<Text>();
+    }
+}
--- Assets/Scripts/HintsManager.cs
+++ Assets/Scripts/HintsManager.cs
@@ -6,19 +6,33 @@ using System.Threading.Tasks;
 
 public class HintsManager : MonoBehaviour
 {
-    //public static HintsManager instance;
-    public Dropdown BuildingsDropdown;
-    public Canvas canvasHints;
-    List<Dictionary<string,object>> BuildingsList = new List<Dictionary<string,object>>();
+    public static HintsManager instance;
 
+    public GameObject hintPrefab;
+    public Transform hintContent;
+    public Canvas canvasConfigHints;
+    //public Dropdown BuildingsDropdown;
+    List<Dictionary<string,object>> BuildingsList = new List<Dictionary<string,object>>();
+    List<Dictionary<string,object>> hintsList = new List<Dictionary<string,object>>();
+    public List<GameObject> currentHints = new List<GameObject>();
+
+
+    int currentSizeHints = 0, newSizeHints = 0;
+
+    void Awake() {
+        instance = this;
+    }
+    
     // Nombre de la DB - Buildings
     async void Start()
     {
-        await SearchBuildingByCollection();
+        //await SearchBuildingByCollection();
     }
 
-    void Update()
+    async void Update()
     {
+        if (canvasConfigHints.enabled)
+            await SearchHint();
     }
 
     async Task SearchBuildingByCollection(){
@@ -28,10 +42,47 @@ public class HintsManager : MonoBehaviour
                 if(pair.Key == "name"){
                     Dropdown.OptionData option = new Dropdown.OptionData();
                     option.text = pair.Value.ToString();
-                    BuildingsDropdown.options.Add(option);
+                    //BuildingsDropdown.options.Add(option);
                 }
             }
         }
     }
 
+    async Task SearchHint()
+    {
+
+        hintsList = await DataBaseManager.instance.SearchByCollection("Hints");
+        newSizeHints = hintsList.Count;
+
+        if (currentSizeHints != newSizeHints)
+        {
+            // Vaciar lista y borrar pistas actuales
+            foreach(GameObject hint in currentHints)
+            {
+                Destroy(hint);
+            }
+            currentHints.Clear();
+
+            foreach(Dictionary<string,object> hint in hintsList)
+            {
+                foreach(KeyValuePair<string,object> pair in hint)
+                {
+                    if(pair.Key == "name"){
+
+                        // Instanciar prefab
+                        GameObject hintElement = Instantiate (hintPrefab, new Vector3(hintContent.position.x,hintContent.position.y, hintContent.position.z) , Quaternion.identity);
+                        hintElement.transform.parent = hintContent.transform;
+                        
+                        // Editar texto
+                        Text hintText = hintPrefab.GetComponentInChildren<Text>();
+                        hintText.text = pair.Value.ToString();
+
+                        // Añadir a Lista
+                        currentHints.Add(hintElement);
+                    }
+                }
+            }
+            currentSizeHints = newSizeHints;
+        }       
+    }
 }
