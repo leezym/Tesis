3c0d4569a2f646463040bc1f94a02eef8f116fe5
DESKTOP-8A1G7ND\totor <totoroldan12@gmail.com>
2021-01-27 20:55:38 -0500

    design: se hicieron unos pequeños cambios en el script kevinNotifications. Además, se importó el asset de Vuforia en el proyecto. NOTA: puede que se encuentren problemas con los otros packages.

--- Assets/Components/Notifications/KevinNotification.cs
+++ Assets/Components/Notifications/KevinNotification.cs
@@ -1,3 +1,4 @@
+using Random=System.Random;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Threading.Tasks;
@@ -9,14 +10,12 @@ public class KevinNotification : MonoBehaviour
     public static KevinNotification instance;
     public Canvas canvasKevinQuotes;
     public Text kevinQuote;
+
     public int correctAnswer;
     private string quote;
 
-    /*private List<string> SuccessQuotesList = new List<string>();
-    private List<string> FailQuotesList = new List<string>();*/
-
-    private var SuccessQuotesList = new List<string>{"Buena esa, crack.", "Eres un Máquina", "Eres un Fiera", "Un Titán", "Caballo"};
-    private var FailQuotesList = new List<string>{"Mejor suerte para la próxima, crack.", "Lamenteibol.", "Llórelo papá."};
+    private List<string> SuccessQuotesList = new List<string>{"Buena esa, crack.", "Eres un Máquina", "Eres un Fiera", "Un Titán", "Caballo"};
+    private List<string> FailQuotesList = new List<string>{"Mejor suerte para la próxima, crack.", "Lamenteibol.", "Llórelo papá."};
 
 
     void awake()
@@ -34,7 +33,7 @@ public class KevinNotification : MonoBehaviour
     private void Update()
     {
         if(kevinQuote.text != ""){
-            LoadKevinQuoteCanvas(kevinQuote.text);
+            LoadKevinQuoteCanvas();
         }
     }
 
